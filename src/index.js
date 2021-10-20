a1ac57e2ed6e1c0dd25c4593522d7a00588c94a8
Carson Johnson <carson74johnson@gmail.com>
2021-10-19 23:45:02 -0400

    added an async await function to clean up the code as well as object deconstruction for the answer to the prompt

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-var inquirer = require('inquirer');
-var jest = require('jest');
+var inquirer = require("inquirer");
+var jest = require("jest");
 var fs = require("fs");
-const questions = require("./questions")
+const {
+  managerQuestions,
+  engineerQuestions,
+  internQuestions,
+} = require("./questions");
 const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
 
 menu = () => {
-    inquirer.prompt(questions.managerQuestions).then(({name, id, email, officeNumber}) => {
-        const manager = new Manager(name, id, email, officeNumber);
-        console.log(manager)
-    })
-}
+  createManager = async () => {
+    const {name, id, email, officeNumber} = await inquirer.prompt(managerQuestions);
+    console.log({name, id, email, officeNumber});
+    const manager = new Manager(name, id, email, officeNumber);
+    console.log(manager);
+  };
+  createManager();
+};
 
 menu();
