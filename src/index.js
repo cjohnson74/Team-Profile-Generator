1bff6353bdd51e7ea7469d476c63fa2004761160
Carson Johnson <carson74johnson@gmail.com>
2021-10-20 17:10:58 -0400

    Added menus for this different quesitons

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const {
   managerQuestions,
   engineerQuestions,
   internQuestions,
+  pickerQuestion
 } = require("./questions");
 const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
@@ -19,5 +20,44 @@ menu = () => {
   };
   createManager();
 };
+
+menuRolePicker = () => {
+  const pick = await inquirer.prompt(pickerQuestion);
+  console.log(pick);
+  switch (pick) {
+    case "Engineer":
+      menuIntern();
+      break;
+    case "Intern":
+      menuEngineer();
+      break;
+    case "Finish building my team":
+      renderHTML();
+      break;
+  }
+}
+
+menuEngineer = () => {
+  menuRolePicker();
+  createEngineer = async () => {
+    const {name, id, email, github} = await inquirer.prompt(engineerQuestions);
+    console.log({name, id, email, github});
+    const engineer = new Engineer(name, id, email, github);
+    console.log(engineer);
+  };
+  createEngineer();
+  menuRolePicker();
+}
+
+menuIntern = () => {
+  createIntern = async () => {
+    const {name, id, email, school} = await inquirer.prompt(internQuestions);
+    console.log({name, id, email, school});
+    const intern = new Intern(name, id, email, school);
+    console.log(intern);
+  };
+  createIntern();
+  menuRolePicker();
+}
 
 menu();
--- src/questions.js
+++ src/questions.js
@@ -4,7 +4,7 @@ managerQuestions : [
     {
         type: "input",
         name: "name",
-        message: "Enter the managers name",
+        message: "Enter the manager's name: ",
         // Validation here
     },
     {
@@ -24,6 +24,62 @@ managerQuestions : [
         name: "officeNumber",
         message: "What is the team manager's office number?",
         // Validation here
+    }],
+engineerQuestions : [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the engineer's name: ",
+        // Validation here
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?",
+        // Validation here
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+        // Validation here
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's github?",
+        // Validation here
+    }],
+internQuestions : [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the intern's name: ",
+        // Validation here
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?",
+        // Validation here
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?",
+        // Validation here
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?",
+        // Validation here
+    }],
+pickerQuestion : [
+    {
+        type: "list",
+        name: "role",
+        message: "Whould you like to add an Engineer or Intern?",
+        choices: ['Engineer', 'Intern', 'Finish building my team']
     }]
-
 }
