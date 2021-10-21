329ca3fc9e7a35f31d3701cf45e781d94f53c5df
Carson Johnson <carson74johnson@gmail.com>
2021-10-20 22:46:05 -0400

    card html is able to be filled with user input

--- src/index.js
+++ src/index.js
@@ -5,63 +5,71 @@ const {
   managerQuestions,
   engineerQuestions,
   internQuestions,
-  pickerQuestion
+  pickerQuestion,
 } = require("./questions");
 const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
+const cardHTML = require("../lib/cardHTML");
 
 menu = () => {
   createManager = async () => {
-    const {name, id, email, officeNumber} = await inquirer.prompt(managerQuestions);
-    console.log({name, id, email, officeNumber});
+    const { name, id, email, officeNumber } = await inquirer.prompt(
+      managerQuestions
+    );
+    console.log({ name, id, email, officeNumber });
     const manager = new Manager(name, id, email, officeNumber);
     console.log(manager);
+    const card = cardHTML.managerCardHTML(manager);
+    console.log(card);
   };
   createManager();
 };
 
 menuRolePicker = () => {
-  const pick = await inquirer.prompt(pickerQuestion);
-  console.log(pick);
-  switch (pick) {
-    case "Engineer":
-      menuIntern();
-      break;
-    case "Intern":
-      menuEngineer();
-      break;
-    case "Finish building my team":
-      renderHTML();
-      break;
-  }
-}
+  pickNext = async () => {
+    const pick = await inquirer.prompt(pickerQuestion);
+    console.log(pick);
+    switch (pick) {
+      case "Engineer":
+        menuIntern();
+        break;
+      case "Intern":
+        menuEngineer();
+        break;
+      case "Finish building my team":
+        renderHTML();
+        break;
+    }
+  };
+};
 
 menuEngineer = () => {
-  menuRolePicker();
   createEngineer = async () => {
-    const {name, id, email, github} = await inquirer.prompt(engineerQuestions);
-    console.log({name, id, email, github});
+    const { name, id, email, github } = await inquirer.prompt(
+      engineerQuestions
+    );
+    console.log({ name, id, email, github });
     const engineer = new Engineer(name, id, email, github);
     console.log(engineer);
   };
   createEngineer();
   menuRolePicker();
-}
+};
 
 menuIntern = () => {
   createIntern = async () => {
-    const {name, id, email, school} = await inquirer.prompt(internQuestions);
-    console.log({name, id, email, school});
+    const { name, id, email, school } = await inquirer.prompt(internQuestions);
+    console.log({ name, id, email, school });
     const intern = new Intern(name, id, email, school);
     console.log(intern);
   };
   createIntern();
   menuRolePicker();
-}
+};
 
 renderHTML = () => {
-  fs.writeF
-}
+  fs.writeF;
+};
 
 menu();
